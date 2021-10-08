6b3725032dcec27a5e7092a3d6ae1e111911ca24
MillicentMalinga <m.malinga@alustudent.com>
2021-10-09 00:20:37 +0200

    Update FrequentExpenses.py

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -1,13 +1,12 @@
 from . import Expense
-from . import data
+
 
 
 expenses = Expense.Expenses()
-expenses.read_expenses(data.spending_data.csv)
+expenses.read_expenses("data/spending_data.csv")
 
 spending_categories = []
 
 for expense in expenses.list:
-    spending_categories = spending_categories.append(expense.category)
-
+    spending_categories.append(expense.category)
     
