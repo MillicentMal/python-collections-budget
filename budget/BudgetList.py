31856e595cf961e60c5616c7538512665a260825
MillicentMalinga <m.malinga@alustudent.com>
2021-10-09 01:02:08 +0200

    Update BudgetList.py

---
+++ budget/BudgetList.py
@@ -0,0 +1,13 @@
+class BudgetList:
+    def __init__(self, budget):
+        self.budget = budget
+        self.sum_expenses = 0
+        self.expenses = list()
+        self.sum_overages = 0
+        self.overages = list()
+
+    def append(self, item):
+        if self.sum_expenses + item < self.budget:
+            self.expenses.append(item)
+            self.sum_expenses + item
+            
